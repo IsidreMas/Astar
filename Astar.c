0311ba4ce36e1d72727fdb7bc80c3c8984960b28
IsidreMas <72302656+IsidreMas@users.noreply.github.com>
2021-10-18 18:22:01 +0200

    Working version
    This version is updated and seems to work fine, writing from graph_builder.c and reading from Astar.c :)

--- Astar.c
+++ Astar.c
@@ -2,19 +2,12 @@
 # include <stdlib.h>
 # include <string.h>
 
-# define MAXSUCCESSORS 9
-
-typedef struct{
-    char *wayname; // Street Id (could be the name)
-    unsigned long nodeposition; // Position in nodes vector of the destination of the successors
-}successorsinfo;
-
 typedef struct {
 unsigned long id; // Node identification
 char *name;
 double lat, lon; // Node position
 unsigned short nsucc; // Number of node successors; i. e. length of successors
-successorsinfo successors[MAXSUCCESSORS];
+unsigned long *successors;
 } node;
 
 void ExitError(const char *miss, int errcode) {
@@ -24,40 +17,48 @@ fprintf (stderr, "\nERROR: %s.\nStopping...\n\n", miss); exit(errcode);
 int main (int argc, char *argv[])
 {
     FILE *fin;
-    unsigned long nnodes, ntotnsucc=0UL, i;
+    unsigned long nnodes, ntotnsucc, ntotnamechar, *allsuccessors,  i;
+    char *allnames;
     node *nodes;
-
     if ((fin = fopen ("Graph.bin", "rb")) == NULL)
-    ExitError("the data file does not exist or cannot be opened", 11);
+        ExitError("the data file does not exist or cannot be opened", 11);
     /* Global data −−− header */
-    if( fread(&nnodes, sizeof(unsigned long), 1, fin) + fread(&ntotnsucc, sizeof(unsigned long), 1, fin) != 2 )
-    ExitError("when reading the header of the binary data file", 12);
+    if( fread(&nnodes, sizeof(unsigned long), 1, fin) + fread(&ntotnsucc, sizeof(unsigned long), 1, fin) + fread(&ntotnamechar, sizeof(unsigned long), 1, fin) != 3 )
+        ExitError("when reading the header of the binary data file", 12);
     /* getting memory for all data */
-    if((nodes=(node *)malloc(nnodes*sizeof(node)))==NULL)
-    ExitError("when allocating memory for the nodes vector", 13);
-    /*if( (
-    allsuccessors = (unsigned long *) malloc(ntotnsucc*sizeof(unsigned long))
-    ) == NULL)
-    ExitError("when allocating memory for the edges vector", 15);
+    if((nodes = (node *) malloc(nnodes*sizeof(node))) == NULL)
+        ExitError("when allocating memory for the nodes vector", 13);
+    if((allsuccessors = (unsigned long *) malloc(ntotnsucc*sizeof(unsigned long)) ) == NULL)
+        ExitError("when allocating memory for the edges vector", 15);
+    if((allnames = (char *) malloc(ntotnamechar*sizeof(char)) ) == NULL)
+        ExitError("when allocating memory for the edges vector", 15);
     /* Reading all data from file */
     if( fread(nodes, sizeof(node), nnodes, fin) != nnodes )
-    ExitError("when reading nodes from the binary data file", 17);
-    /* if(fread(allsuccessors, sizeof(unsigned long), ntotnsucc, fin) != ntotnsucc)
-    ExitError("when reading sucessors from the binary data file", 18);*/
+        ExitError("when reading nodes from the binary data file", 17);
+    if(fread(allsuccessors, sizeof(unsigned long), ntotnsucc, fin) != ntotnsucc)
+        ExitError("when reading sucessors from the binary data file", 18);
+    if(fread(allnames, sizeof(char), ntotnamechar, fin) != ntotnamechar)
+        ExitError("when reading names from the binary data file", 18);
     fclose(fin);
-    /* Setting pointers to successors */
-    /*for(i=0; i < nnodes; i++) if(nodes[i].nsucc) {
-    nodes[i].successors = allsuccessors; allsuccessors += nodes[i].nsucc;
-    }*/
-
-    printf("%lu", nnodes);
-
-    printf("Printing the nodes with at least one successors:\n");
-    for(i=0;i<nnodes;i++){
-        if(nodes[i].nsucc != 0){
-            printf("Id=%010ld Lat=%lf Long=%lf Number_of_successorss=%d Id_of_successor_1=%lu\n",nodes[i].id,nodes[i].lat,nodes[i].lon,nodes[i].nsucc,nodes[i].successors[2].nodeposition);
+    /* Setting pointers to successors and names*/
+    for(i=0UL; i < nnodes; i++)
+    {
+        if(nodes[i].nsucc)
+        {
+            nodes[i].successors = allsuccessors;
+            allsuccessors += nodes[i].nsucc;
         }
+        nodes[i].name = allnames;
+        allnames += strlen(allnames)+1;
     }
+        
+        printf("Printing the nodes with at least one successors:\n");
+        for(i=0;i<nnodes;i++){
+            if(nodes[i].nsucc != 0 && strcmp(nodes[i].name, (char*)"")){
+                printf("Id=%010ld Lat=%lf Long=%lf Number_of_successorss=%d Id_of_successor_1=%lu Name=%s\n",nodes[i].id,nodes[i].lat,nodes[i].lon,nodes[i].nsucc,nodes[i].successors[0], nodes[i].name);
+            }
+        }
+
 
     return 0;
 }
--- graph_builder.c
+++ graph_builder.c
@@ -1,153 +1,154 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <stdbool.h>
 
-# define MAXSUCCESSORS 9
-
-typedef struct{
-    char *wayname; // Street Id (could be the name)
-    unsigned long nodeposition; // Position in nodes vector of the destination of the successors
-}successorsinfo;
+# define MAXCHARSLINE 79857
 
 typedef struct {
 unsigned long id; // Node identification
-char *name;
-double lat, lon; // Node position
+char *name; // Node name
+double lat, lon; // Node geographical position
 unsigned short nsucc; // Number of node successors; i. e. length of successors
-successorsinfo successors[MAXSUCCESSORS];
+unsigned long *successors; // Node successors
 } node;
 
-unsigned long binarysearch(unsigned long ident, node l[],int n, unsigned long max_id); // Performs a binary search.
+// Performs a binary search.
+bool binarysearch(unsigned long ident, node l[],int n, unsigned long *index);
 
-char* strtoke(char *str, const char *delim); // Works as strtok() but returns an empty string (instead of NULL) when the field is empty.
+// Works as strtok() but returns an empty string (instead of NULL) when the field is empty.
+char* strtoke(char *str, const char *delim);
 
-void ExitError(const char *miss, int errcode); // Handles errors and exits the program in case of one.
+// Handles errors and exits the program in case of one.
+void ExitError(const char *miss, int errcode);
 
 int main(int argc, char *argv[]){
     FILE *nodesdata;
     node *nodes;
-    unsigned long nnodes=0,i,buffer_length, field;
-    char buffer[79858], *pch;
+    unsigned long nnodes=0UL,i, field;
+    char buffer[MAXCHARSLINE], *pch;
     
     nodesdata=fopen("maps_data/cataluna.csv","r");
     if(nodesdata==NULL)
     {
-        printf("\nWe cannot acces to the nodes data file\n");
-        return 1;
+        ExitError("Couldn't acces the nodes data file", 32);
     }
-    // We count the number of nodes
-
+    
+    // Counts the number of nodes
     while(fgets(buffer, sizeof(buffer), nodesdata))
     {
         pch = strtoke(buffer,"|");
-        if (strcmp(pch, (char *)"node") == 0) {
-            nnodes++;
-        }
+        if (strcmp(pch, (char *)"node") == 0)nnodes++;
+        else if(strcmp(pch, (char *)"way") == 0)break;
     }
-
     rewind(nodesdata);
-    // Reserve the memory for the nodes.
+
+    // Reserves the memory for the nodes.
     if((nodes=(node *)malloc(nnodes*sizeof(node)))==NULL)
-        ExitError("in the memory allocation of nodes", 32);
-    // Skip the first lines that are not nodes.
+        ExitError("Couldn't allocate the memory for nodes", 32);
+
+    // Skips the first lines that are not nodes.
     do
     {
         fgets(buffer, sizeof(buffer), nodesdata); 
         pch = strtoke(buffer,"|");
     }while(strcmp(pch, (char *)"node") != 0);
 
-    // Read the data in the nodes lines and store it in the vector nodes.
-    for(i=0;i<nnodes;i++)
+    // Reads the data in the nodes lines and store it in the vector nodes.
+    for(i=0UL;i<nnodes;i++)
     {
-        field = 1;
+        field = 1UL;
         while (pch != NULL)
         {
-            if(field == 2)nodes[i].id = strtoul(pch,(char **)NULL, 10);
-            if(field == 3)nodes[i].name = pch; //This needs to be fixed!!
-            if(field == 10)nodes[i].lat = atof(pch);
-            if(field == 11)nodes[i].lon = atof(pch);
+            if(field == 2UL)nodes[i].id = strtoul(pch,(char **)NULL, 10);
+            if(field == 3UL)
+            {   
+                if((nodes[i].name = (char *) malloc(strlen(pch)*sizeof(char))) == NULL)
+                    ExitError("Couldn't allocate the memory for names", 32);
+                strcpy(nodes[i].name, pch);
+            }
+            if(field == 10UL)nodes[i].lat = atof(pch);
+            if(field == 11UL)nodes[i].lon = atof(pch);
             field++;
             pch = strtoke(NULL, "|");
         }
-        nodes[i].nsucc=0;
+        nodes[i].nsucc=0U;
+        if((nodes[i].successors=(unsigned long *)malloc(nodes[i].nsucc*sizeof(unsigned long)))==NULL)
+            ExitError("Couldn't allocate the memory for successors", 32);
+
         fgets(buffer, sizeof(buffer), nodesdata);
-        if(i==nnodes-1)buffer_length=strlen(buffer);
         pch = strtoke(buffer,"|");
     }
 
-    fseek(nodesdata, -buffer_length, SEEK_CUR); // Rewinds the last line read from the file.
-
-    // Now we have to read the nodes of the street and keep two in memory: previousnodeid and nodeid.
-    unsigned long nodeid, previousnodeid, wayid, max_id = nodes[nnodes-1].id+1, previousnode, previousnodeposition;
-    short flag, oneway; //This are flags to deal with the inconsistencies in the file and reproduce the successors in twoway roads.
-    char *wayname;
+    // Reads the consecutive nodes of the way lines and keep two in memory: previousnodeindex and nodeindex.
+    unsigned long nodeid, previousnodeid, nodeindex, previousnodeindex;
+    bool first_node, repeated_node, oneway; //This are flags to deal with the inconsistencies in the file and reproduce the successors in twoway roads.
 
     while(strcmp(pch, (char *)"way") == 0)
     {
-        flag = 1;
-        field = 1;
-        oneway = 0;
-        while (pch != NULL)
+        first_node = true;
+        field = 1UL;
+        oneway = false;
+        // Skips the first fields before the node members of the way and sets the flag oneway if it's true.
+        while(pch!= NULL && field < 10UL)
         {
-            if(field == 2)wayid = strtoul(pch,(char **)NULL, 10);
-            if(field == 3)wayname = pch;
-            if(field == 8)if(strcmp(pch, (char *)"oneway")==0)oneway = 1;
-            if(field >= 10)
-            {   
-                if(flag==1)
+            if(field == 8UL)if(strcmp(pch, (char *)"oneway")==0)oneway = true;
+            field++;
+            pch = strtoke(NULL, "|");
+        }
+        // Deals with the inconsistencies of the file and properly stores the successors of each node.
+        while (pch != NULL)
+        {     
+            if(first_node)
+            {
+                previousnodeid = strtoul(pch,(char **)NULL, 10);
+                if(!binarysearch(previousnodeid, nodes, nnodes, &previousnodeindex))
                 {
-                    flag = 0;
-                    previousnodeid = strtoul(pch,(char **)NULL, 10);
-                    if((previousnodeposition = binarysearch(previousnodeid, nodes, nnodes, max_id)) == max_id)
-                    {
-                        flag = 1;
-                        field++;
-                        pch = strtoke(NULL, "|");
-                        continue;
-                    }
-                    field++;
-                    pch = strtoke(NULL, "|");
-                    if (pch == NULL)break;// ways with just one node are discarded automatically because field=11 is NULL
-                }
-                nodeid = strtoul(pch,(char **)NULL, 10);
-                if((previousnode = binarysearch(nodeid, nodes, nnodes, max_id)) == max_id)
-                {
-                    flag = 1;
                     field++;
                     pch = strtoke(NULL, "|");
                     continue;
                 }
-
-                if(flag == 0)
-                {
-                    for(i=0;i<nodes[previousnodeposition].nsucc;i++)
-                    {
-                        if(nodes[previousnodeposition].successors[i].nodeposition==previousnode)flag = 1;
-                    }
-                    if(flag == 0)
-                    {
-                        nodes[previousnodeposition].successors[nodes[previousnodeposition].nsucc].nodeposition = previousnode;
-                        nodes[previousnodeposition].successors[nodes[previousnodeposition].nsucc].wayname = wayname;
-                        nodes[previousnodeposition].nsucc++;
-                    }
-                    if(oneway == 0)
-                    {
-                        for(i=0;i<nodes[previousnode].nsucc;i++)
-                        {
-                            if(nodes[previousnode].successors[i].nodeposition==previousnodeposition)flag = 1;
-                        }
-                        if(flag == 0)
-                        {
-                            nodes[previousnode].successors[nodes[previousnode].nsucc].nodeposition=previousnodeposition;
-                            nodes[previousnode].successors[nodes[previousnode].nsucc].wayname = wayname;
-                            nodes[previousnode].nsucc++;
-                        }
-                    }
-                    previousnodeposition=previousnode;
-                    flag = 0;
-                }
-
+                first_node = false;
+                field++;
+                pch = strtoke(NULL, "|");
+                continue;
             }
+            nodeid = strtoul(pch,(char **)NULL, 10);
+            if(!binarysearch(nodeid, nodes, nnodes, &nodeindex))
+            {
+                first_node = true;
+                field++;
+                pch = strtoke(NULL, "|");
+                continue;
+            }
+            repeated_node=false;
+            for(i=0;i<nodes[previousnodeindex].nsucc;i++)
+            {
+                if(nodes[previousnodeindex].successors[i]==nodeindex)repeated_node = true;
+            }
+            if(!repeated_node)
+            {
+                if((nodes[previousnodeindex].successors=(unsigned long *)realloc(nodes[previousnodeindex].successors, (nodes[previousnodeindex].nsucc + 1u)* sizeof(unsigned long)))==NULL)
+                    ExitError("Couldn't reallocate the memory for successors", 32);
+                nodes[previousnodeindex].successors[nodes[previousnodeindex].nsucc] = nodeindex;
+                nodes[previousnodeindex].nsucc++;
+            }
+            if(!oneway)
+            {
+                repeated_node = false;
+                for(i=0;i<nodes[nodeindex].nsucc;i++)
+                {
+                    if(nodes[nodeindex].successors[i]==previousnodeindex)repeated_node = true;
+                }
+                if(!repeated_node)
+                {
+                    if((nodes[nodeindex].successors=(unsigned long *)realloc(nodes[nodeindex].successors, (nodes[nodeindex].nsucc + 1u)* sizeof(unsigned long)))==NULL)
+                    ExitError("Couldn't reallocate the memory for successors", 32);
+                    nodes[nodeindex].successors[nodes[nodeindex].nsucc]=previousnodeindex;
+                    nodes[nodeindex].nsucc++;
+                }
+            }
+            previousnodeindex=nodeindex;
             field++;
             pch = strtoke(NULL, "|");
         }
@@ -156,83 +157,81 @@ int main(int argc, char *argv[]){
     }
 
     fclose(nodesdata);
-    // Let's show the result
-    printf("Printing the result of reading the nodes file:\n");
-    for(i=1;i<nnodes;i++){
-        if(nodes[i-1].id>nodes[i].id)printf("FAIL");//This checks that the ID's are sorted (necessary to perform binary search)
 
-        //printf("Id=%010ld Lat=%lf Long=%lf\n",nodes[i].id,nodes[i].lat,nodes[i].lon);
+    // Error checker (This can be commented out)
+    printf("Checking if there were errors reading the file...\n");
+    for(i=1;i<nnodes;i++){
+        if(nodes[i-1].id>nodes[i].id)
+            ExitError("The nodes id's are not sorted in ascending order. That's necessary to perform binary search.", 32);//This checks that the ID's are sorted (necessary to perform binary search)
     }
     
-    // We print the nodes with at least one successors to control if it works
+    // We print the nodes with at least one successor to control if it works
     printf("Printing the nodes with at least one successors:\n");
     for(i=0;i<nnodes;i++){
-        if(nodes[i].nsucc != 0){
-            //printf("Id=%010ld Lat=%lf Long=%lf Number_of_successorss=%d Id_of_successor_1=%lu Name=%s\n",nodes[i].id,nodes[i].lat,nodes[i].lon,nodes[i].nsucc,nodes[i].successors[nodes[i].nsucc-1].nodeposition, nodes[i].name);
+        if(nodes[i].nsucc != 0U && strcmp(nodes[i].name, (char*)"")){
+            printf("Id=%010ld Lat=%lf Long=%lf Number_of_successorss=%d Id_of_successor_1=%lu Name=%s\n",nodes[i].id,nodes[i].lat,nodes[i].lon,nodes[i].nsucc,nodes[i].successors[0], nodes[i].name);
         }
     }
 
-    // Writing the binary file to be used in the Astar.c program
-
+    // Writes the binary file with the formatted graph, names and successors are writen in a single vector and need to be reasigned with pointers when reading.
     FILE *fin;
-    // Computing the total number of successors
+
+    /* Computing the total number of successors */
     unsigned long ntotnsucc=0UL;
-
-    for(i=0; i < nnodes; i++) ntotnsucc += nodes[i].nsucc;
-
+    for(i=0UL; i < nnodes; i++) ntotnsucc += nodes[i].nsucc;
+    /* Computing the total number of characters in names */
+    unsigned long ntotnamechar=0UL;
+    for(i=0UL; i < nnodes; i++) ntotnamechar += strlen(nodes[i].name)+1;
+    /* Setting the name of the binary file */
     if ((fin = fopen ("Graph.bin", "wb")) == NULL)
-    ExitError("the output binary data file cannot be opened", 31);
-    printf("%lu", nnodes);
-    //Global data −−− header
-    if( fwrite(&nnodes, sizeof(unsigned long), 1, fin) + fwrite(&ntotnsucc, sizeof(unsigned long), 1, fin) != 2 )
-    ExitError("when initializing the output binary data file", 32);
-
-    //Writing all nodes
+        ExitError("the output binary data file cannot be opened", 31);
+    /* Global data −−− header */
+    if( fwrite(&nnodes, sizeof(unsigned long), 1, fin) + fwrite(&ntotnsucc, sizeof(unsigned long), 1, fin) + fwrite(&ntotnamechar, sizeof(unsigned long), 1, fin)!= 3 )
+        ExitError("when initializing the output binary data file", 32);
+    /* Writing all nodes */
     if( fwrite(nodes, sizeof(node), nnodes, fin) != nnodes )
-    ExitError("when writing nodes to the output binary data file", 32);
-
-    /*//Writing sucessors in blocks
-    for(i=0; i < nnodes; i++) if(nodes[i].nsucc)
-    {
-        if( fwrite(nodes[i].successors, sizeof(unsigned long), nodes[i].nsucc, fin) !=
-        nodes[i].nsucc )
+        ExitError("when writing nodes to the output binary data file", 32);
+    /* Writing sucessors in blocks */
+    for(i=0UL; i < nnodes; i++)if(nodes[i].nsucc)if( fwrite(nodes[i].successors, sizeof(unsigned long), nodes[i].nsucc, fin) != nodes[i].nsucc )
         ExitError("when writing edges to the output binary data file", 32);
-    }*/
+    /* Writing names in blocks */
+    for(i=0UL; i < nnodes; i++)if( fwrite(nodes[i].name, sizeof(char), strlen(nodes[i].name)+1, fin) != strlen(nodes[i].name)+1)
+        ExitError("when writing edges to the output binary data file", 32);
+
     fclose(fin);
+    free(nodes);
 
     return 0;
 }
 
-unsigned long binarysearch(unsigned long ident, node l[],int n, unsigned long max_id)
+bool binarysearch(unsigned long ident, node l[],int n, unsigned long *index)
 {
     unsigned long middle, start = 0, end = n-1;
-    while (start <= end){
-      middle = start + (end- start)/2;
+    while (start <= end)
+    {
+      middle = (end + start)/2;
       if (l[middle].id == ident)
-         return middle;
+      {
+          *index=middle;
+          return true;
+      }
       if (l[middle].id < ident)
          start = middle + 1;
       else
          end = middle - 1;
-   }
-   return max_id;
+    }
+    return false;
 }
 
 char* strtoke(char *str, const char *delim)
 {
-  static char *start = NULL; /* stores string str for consecutive calls */
-  char *token = NULL; /* found token */
-  /* assign new start in case */
+  static char *start = NULL;
+  char *token = NULL;
   if (str) start = str;
-  /* check whether text to parse left */
   if (!start) return NULL;
-  /* remember current start as found token */
   token = start;
-  /* find next occurrence of delim */
   start = strpbrk(start, delim);
-  /* replace delim with terminator and move start to follower */
   if (start) *start++ = '\0';
-  /* done */
   return token;
 }
 
