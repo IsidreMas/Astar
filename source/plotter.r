79bce13897df597bbbdedf61e68d0140ebd51178
mdomhoefer <martha.domhoefer@gmail.com>
2021-12-09 20:03:49 +0100

    r

--- source/plotter.r
+++ source/plotter.r
@@ -1,9 +1,13 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
+print(args)
 
 install.packages('leaflet')
 library(leaflet)
 library(htmlwidgets)
 
 (m<-leaflet(data = read.csv(paste("./results/",args[1],"_",args[2],"_",args[3],".csv", sep="", collapse=NULL)))%>%addTiles()%>%addPolylines(lat = ~lat, lng = ~lon, color = args[4]))
+#(m<-leaflet(data = read.csv(paste("./results/","Astar","_","240949599","_","195977239",".csv", sep="", collapse=NULL)))%>%addTiles()%>%addPolylines(lat = ~lat, lng = ~lon, color ="red"))
+
 saveWidget(m, file=paste("./plots/",args[1],"_",args[2],"_",args[3],".html", sep="", collapse=NULL))
+
